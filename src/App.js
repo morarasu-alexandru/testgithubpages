15bc05b9945e42b2877416c5bad21db888c40f46
Alexandru Morarasu <morarasu_alexandru@yahoo.com>
2020-06-09 20:02:20 +0300

    'package'

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Very nice app to be posted on Github pages</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
