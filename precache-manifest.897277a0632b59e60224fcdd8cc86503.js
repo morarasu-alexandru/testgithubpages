50c65d5955908304d36a54a1dcc4cf31e337220b
Alexandru Morarasu <morarasu_alexandru@yahoo.com>
2020-06-09 20:12:30 +0300

    Updates

---
+++ precache-manifest.897277a0632b59e60224fcdd8cc86503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b86453cb29f2607da03046f083370c",
+    "url": "/testgithubpages/index.html"
+  },
+  {
+    "revision": "49f8f7007f0467f69806",
+    "url": "/testgithubpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05801b271069a1a86809",
+    "url": "/testgithubpages/static/js/2.12946ff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testgithubpages/static/js/2.12946ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f8f7007f0467f69806",
+    "url": "/testgithubpages/static/js/main.6815c325.chunk.js"
+  },
+  {
+    "revision": "67feb5a7613fd6064b90",
+    "url": "/testgithubpages/static/js/runtime-main.7fedb5bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testgithubpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/morarasu-alexandru.github.io/testgithubpages/precache-manifest.36c8bdb49f1cb95ad3c2c6d3f341ea20.js"
+  "/testgithubpages/precache-manifest.897277a0632b59e60224fcdd8cc86503.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/morarasu-alexandru.github.io/testgithubpages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testgithubpages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
