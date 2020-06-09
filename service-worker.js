a7629ecde2db1f6b5d50d3b5b730ee0006df8c91
Alexandru Morarasu <morarasu_alexandru@yahoo.com>
2020-06-09 20:10:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e8e8796bc7c366df9d1666a85b0014f.js"
+  "/morarasu-alexandru.github.io/testgithubpages/precache-manifest.36c8bdb49f1cb95ad3c2c6d3f341ea20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/morarasu-alexandru.github.io/testgithubpages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
